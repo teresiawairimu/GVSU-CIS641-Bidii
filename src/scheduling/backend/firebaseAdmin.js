76a504f0d753990c308fb97e826af533c7adac51
teresiawairimu <teresiawairimu06.gmail.com>
2024-10-22 21:22:49 -0400

    stripe configuration

---
+++ src/scheduling/backend/firebaseAdmin.js
@@ -0,0 +1,10 @@
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+module.exports = {db, admin};
